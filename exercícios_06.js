dad701b064290dd35df2bbddefc6b36a4c455215
davizilz-2213 <davi_zilz@estudante.sesisenai.org.br>
2026-03-24 07:56:22 -0300

    add-

---
+++ exercícios_06.js
@@ -0,0 +1,15 @@
+function VogalConsoante(){
+    let letra = prompt ("Digite a vogal");
+
+let vogais = ["a", "e", "i", "o", "u" ,"A", "E", "I", "O", "U"];
+
+
+if (vogais.includes(letra)) {
+
+    alert("VOGAL");
+}else{
+    alert("CONSOANTE");
+}
+
+
+}

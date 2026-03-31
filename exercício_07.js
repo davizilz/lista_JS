51252607aace48ecceaf4474ac68d6d51a0bf3f9
davizilz-2213 <davi_zilz@estudante.sesisenai.org.br>
2026-03-31 07:29:52 -0300

    add

---
+++ exercício_07.js
@@ -0,0 +1,42 @@
+function picoleteria(){
+
+    let codigo = prompt("DIGITE O CODIGO DE UM SORVETE, 'a' ate 'i' ");
+    
+    if(codigo =="a"){
+     alert("chocolate R$1,50");
+    }
+    
+    else if (codigo =="b"){
+    alert("Morango R$ 2,50");
+    }
+    
+    else if (codigo =="c"){
+     alert ("Creme R$ 2,50");
+    }
+    
+    else if (codigo =="d"){
+     alert("Manga R$ 3,20");
+    }
+    
+    else if (codigo =="e"){
+    alert ("Melancia R$ 3,40");
+    }
+    
+    else if (codigo =="f"){
+     alert ("Vanilla Ice R$ 3,00");
+    }
+    
+    else if (codigo =="g"){
+    alert ("Céu Azul R$ 3,60");
+    }
+    
+    else if (codigo =="h"){
+     alert ("Brownie R$ 4,00");
+    }
+    
+    else if (codigo =="i"){
+     alert ("Hawaiano R$ 5,00");
+    }
+}
+
+
---
+++ exercício_08.js
@@ -0,0 +1,4 @@
+function variaveis(){
+
+    let numero_1 = 
+}
